d3be7e103d3865893ffd4ef128f1dff5dfe8d832
CAJALEWI <cjlew1234@gmail.com>
2025-10-12 11:55:34 -0700

    Created Fields

---
+++ src/main/java/SlIntArray.java
@@ -0,0 +1,12 @@
+public class SlIntArray {
+    protected int NUM_COLS;
+    protected int[][] arrayData;
+    protected int NUM_ROWS;
+
+    public SlIntArray(int rows, int cols) {
+        NUM_ROWS = rows;
+        NUM_COLS = cols;
+    }
+
+
+}
---
+++ src/main/java/SlPingPongArray.java
@@ -0,0 +1,17 @@
+public class SlPingPongArray {
+    public SlIntArray cellArrayB;
+    public SlIntArray nextCellArray;
+    private final String DEFAULT_OUTPUT_FILE;
+    public SlIntArray cellArrayA;
+    protected int DEFAULT_VALUE;
+    public SlIntArray liveCellArray;
+    public int MAX_VALUE;
+    public final static int ALIVE;
+    public int MIN_VALUE;
+    public final static int DEAD;
+
+    public SlPingPongArray(int rows, int cols, int defaultV, int huh) {
+
+    }
+
+}
