54be445555d804fe49632d13de12de7b898188d4
CAJALEWI <cjlew1234@gmail.com>
2025-10-13 08:54:28 -0700

    Initialized Variables

--- src/main/java/SlPingPongArray.java
+++ src/main/java/SlPingPongArray.java
@@ -1,17 +1,25 @@
 public class SlPingPongArray {
     public SlIntArray cellArrayB;
     public SlIntArray nextCellArray;
-    private final String DEFAULT_OUTPUT_FILE;
+    private final String DEFAULT_OUTPUT_FILE = "OutULTOutput.txt";
     public SlIntArray cellArrayA;
     protected int DEFAULT_VALUE;
     public SlIntArray liveCellArray;
     public int MAX_VALUE;
-    public final static int ALIVE;
+    public final static int ALIVE = 1;
     public int MIN_VALUE;
-    public final static int DEAD;
+    public final static int DEAD = 0;
 
     public SlPingPongArray(int rows, int cols, int defaultV, int huh) {
-
+        this.nextCellArray = new SlIntArray(rows, cols);
+        this.DEFAULT_VALUE = defaultV;
+        this. MIN_VALUE = 0;
+        this. MAX_VALUE = 10;
     }
+
+    public SlPingPongArray(int rows, int cols) {
+        nextCellArray = new SlIntArray(rows, cols);
+    }
+
 
 }
