829723047758ccfe5fac8c9f0caa970a9afd1407
CAJALEWI <cjlew1234@gmail.com>
2025-10-24 20:06:29 -0700

    Removed

--- src/test/java/MainTest.java
+++
@@ -1,7 +0,0 @@
-public class MainTest {
-    public static void main(String[] myArgs) {
-        //SlPingPongArrayTest.ULT_200();
-        //SlPingPongArrayTest.ULT_201();
-        SlPingPongArrayTest.ULT_202();
-    }  //  public static void main(String[] myArgs)
-}
